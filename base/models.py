11cfb78055bf3a0325bd87e8b94e41c13b190832
nitaysason <nitaysason@gmail.com>
2024-02-13 11:44:27 +0200

    first commit

--- base/models.py
+++ base/models.py
@@ -8,6 +8,7 @@ class Librarian(models.Model):
     user =models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     id = models.BigAutoField(primary_key=True)
     book = models.CharField(max_length=50,null=True,blank=True)
+    image = models.ImageField(null=True,blank=True,default='/placeholder.png')
     author = models.CharField(max_length=50,null=True,blank=True)
     createdTime=models.DateTimeField(auto_now_add=True)
     fields =['book','author']
